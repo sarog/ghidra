417713da492a804bffa8e8dac3cc871a80985069
Saro G. <saro@saromedia.com>
2024-12-20 09:56:20 -0500

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,24 @@
+$env:JAVA_HOME = "$env:USERPROFILE\scoop\apps\temurin21-jdk\current"
+# $env:Path = $env:JAVA_HOME + '\bin;' + $env:Path
+
+Write-Host ">>> Java Home: $env:JAVA_HOME"
+
+& "$env:JAVA_HOME\bin\java.exe" -version
+java -version
+
+Write-Host ">>> Fetching dependencies"
+gradle -I gradle/support/fetchDependencies.gradle init
+
+Write-Host ">>> Preparing Ghidra development environment (Eclipse plugin)"
+gradle prepdev cleanEclipse eclipse buildNatives
+gradle sleighCompile
+gradle createJavadocs
+gradle buildPyPackage
+
+Write-Host ">>> Building Ghidra"
+
+## Uncompressed form:
+gradle assembleAll -x ip
+
+## Compressed distribution:
+# gradle buildGhidra -x ip
